21f29ec443ec70912ee68d0770909f4f0de25b58
dsGonz <dsgonza2@illinois.edu>
2019-10-01 17:10:53 -0500

    Made eval_depth script

---
+++ experiments/eval_mono.sh
@@ -0,0 +1,5 @@
+MODEL=ssim_0
+CUDA_VISIBLE_DEVICES=1 python3 ../evaluate_depth.py --eval_mono \
+                        --load_weights_folder ../logs/$MODEL/models/weights_19 \
+                        --split eigen_zhou --data_path /mnt/data0-nfs/shared-datasets/kitti_data \
+                        --png
